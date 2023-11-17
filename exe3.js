eb5e1f07e8bf3e5af4e93078ed17642ca9de0eff
Mariane-Medeiros <150842965+Mariane-Medeiros@users.noreply.github.com>
2023-11-17 15:50:39 -0300

    exe3

---
+++ exe3.js
@@ -0,0 +1,7 @@
+// Crie uma div vazia cujo o id dela é 'resultado' Insira via JavaScript assim que a tela carregar o seguinte texto: 'Esse texto foi inserido pelo JavaScript...' 'Exemplo de "window.onload"'
+
+window.onload = function add_mensagem(){
+    document.getElementById('resultado').innerHTML = 'Esse texto foi inserido pelo JavaScript...';
+}
+
+ 
