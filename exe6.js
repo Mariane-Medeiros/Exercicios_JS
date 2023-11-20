932327a59dd4f2a5e2613ca5c278be6befb652fd
Mariane-Medeiros <150842965+Mariane-Medeiros@users.noreply.github.com>
2023-11-20 17:22:20 -0300

    Update and rename exe6.js to exe6/exe6.js

--- exe6.js
+++
@@ -1,2 +0,0 @@
-//Exiba o 'resto' da seguinte divisão: (5 / 2)
-document.getElementById('resto').innerHTML = 5%2;
---
+++ exe6/exe6.js
@@ -0,0 +1,2 @@
+//Exiba o 'resto' da seguinte divisão: (5 / 2)
+document.getElementById('resto').innerHTML = 5%2;
